c4422f57c143975ba6fcee2af7326ef6728995e5
karafaze <fazli.carrat@gmail.com>
2023-02-24 15:56:48 +0100

    added possibility to keep previous search in a list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import {nanoid} from 'nanoid';
 
 export default function App() {
     const [city, setCity] = useState("");
     const [weatherData, setWeatherData] = useState(null);
+    const [weatherDataList, setWeatherDataList] = useState([])
     const [error, setError] = useState(null)
     const [theme, setTheme] = useState('dark');
 
@@ -18,10 +20,13 @@ export default function App() {
             const data = await response.json();
             if (data.cod === '404'){
                 setWeatherData(null)
-                setError('Sorry the city your entered doet not seem to exists.')
+                setError(`Sorry the city your entered (${cityName}) does not seem to exists.`)
                 setCity('')
             } else {
-                setWeatherData(data);
+                setWeatherData(saveWeatherData(data));
+                setWeatherDataList(prevList => {
+                    return [saveWeatherData(data), ...prevList]
+                })
                 setCity('')
                 setError(null)
             }
@@ -38,8 +43,11 @@ export default function App() {
         return `${clientTime.getHours()}h${clientTime.getMinutes()}`
     };
 
-    const deleteWeatherCar = () => {
-        setWeatherData(null)
+    const deleteWeatherCard = (id) => {
+        setWeatherDataList(prevList => {
+            return prevList.filter(city => city.id !== id)
+        }) 
+        // setWeatherData(null)
     }
 
     const toggleTheme = () => {
@@ -53,6 +61,49 @@ export default function App() {
         marginLeft: theme === 'warm' ? 'auto' : 0,
         padding: theme === 'dark' ? '0 .05em 0 0' : '0 0.05em 0 0',
     }
+
+    const saveWeatherData = (data) => {
+        return {
+            id: nanoid(),
+            city_name: data.name,
+            global_temp: data.weather[0].main,
+            main_temp: data.main.temp.toFixed(1),
+            feels_like: data.main.feels_like.toFixed(1),
+            timezone: data.timezone,
+            wind_speed: data.wind.speed,
+            clouds: data.clouds.all,
+            humidity: data.main.humidity
+        }
+    }
+
+    const weatherDataToHtml = weatherDataList.map((city) => {
+        return (
+            <article key={nanoid()} className={`weather--single ${theme === 'warm' ? 'weather--single__warm': ''}`}>
+                <i 
+                    onClick={() => deleteWeatherCard(city.id)}
+                    className="ri-close-line delete-btn"
+                ></i>
+                <div className={`weather--main ${theme === 'warm' ? 'weather--main__warm': ''}`}>
+                    <p className="weather--main__global">{city.global_temp}</p>
+                    <div className="weather--main__degree">
+                        <p>{city.main_temp}°C</p>
+                        <span>(Feels like : {city.feels_like}°C)</span>
+                    </div>
+                </div>
+                <div className="weather--detail">
+                    <h2 className="weather--city">{city.city_name}</h2>
+                    <h3 className="weather--time">Current time : {getTime(city.timezone)}</h3>
+                </div>
+                <div className={`weather--additionnal ${theme === 'warm' ? 'weather--additionnal__warm': ''}`}>
+                    <p>Wind : {city.wind_speed}</p>
+                    <p>Clouds : {city.clouds}</p>
+                    <p>Humidity : {city.humidity}</p>
+                </div>
+            </article>
+        )
+    })
+
+    document.body.style.backgroundColor = theme === 'dark' ? '#1b262c' : '#B73E3E';
 
     return (
         <main className={`main--container ${theme === 'warm' ? 'main--container__warm': ''}`}>
@@ -81,30 +132,9 @@ export default function App() {
             </div>
             { error ? <p className="error-search">{error}</p> : null}
             {
-                weatherData ? (
+                weatherDataList.length > 0 ? (
                     <section className="weather-list">
-                        <article className={`weather--single ${theme === 'warm' ? 'weather--single__warm': ''}`}>
-                            <i 
-                                onClick={deleteWeatherCar}
-                                class="ri-close-line delete-btn"
-                            ></i>
-                            <div className={`weather--main ${theme === 'warm' ? 'weather--main__warm': ''}`}>
-                                <p className="weather--main__global">{weatherData.weather[0].main}</p>
-                                <div className="weather--main__degree">
-                                    <p>{weatherData.main.temp.toFixed(1)}°C</p>
-                                    <span>(Feels like : {weatherData.main.feels_like.toFixed(1)}°C)</span>
-                                </div>
-                            </div>
-                            <div className="weather--detail">
-                                <h2 className="weather--city">{weatherData.name}</h2>
-                                <h3 className="weather--time">Current time : {getTime(weatherData.timezone)}</h3>
-                            </div>
-                            <div className={`weather--additionnal ${theme === 'warm' ? 'weather--additionnal__warm': ''}`}>
-                                <p>Wind : {weatherData.wind.speed}</p>
-                                <p>Clouds : {weatherData.clouds.all}</p>
-                                <p>Humidity : {weatherData.main.humidity}</p>
-                            </div>
-                        </article>
+                        {weatherDataToHtml}
                     </section>
                 ) : (
                     <p className="empty-search">Type a city in the search bar to get its detail</p>
@@ -113,3 +143,29 @@ export default function App() {
         </main>
     );
 }
+
+
+{/* <section className="weather-list">
+<article className={`weather--single ${theme === 'warm' ? 'weather--single__warm': ''}`}>
+    <i 
+        onClick={deleteWeatherCar}
+        class="ri-close-line delete-btn"
+    ></i>
+    <div className={`weather--main ${theme === 'warm' ? 'weather--main__warm': ''}`}>
+        <p className="weather--main__global">{weatherData.weather[0].main}</p>
+        <div className="weather--main__degree">
+            <p>{weatherData.main.temp.toFixed(1)}°C</p>
+            <span>(Feels like : {weatherData.main.feels_like.toFixed(1)}°C)</span>
+        </div>
+    </div>
+    <div className="weather--detail">
+        <h2 className="weather--city">{weatherData.name}</h2>
+        <h3 className="weather--time">Current time : {getTime(weatherData.timezone)}</h3>
+    </div>
+    <div className={`weather--additionnal ${theme === 'warm' ? 'weather--additionnal__warm': ''}`}>
+        <p>Wind : {weatherData.wind.speed}</p>
+        <p>Clouds : {weatherData.clouds.all}</p>
+        <p>Humidity : {weatherData.main.humidity}</p>
+    </div>
+</article>
+</section> */}
