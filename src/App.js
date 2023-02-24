218431b42c5c0b263dc56ec7f820e75230e40c9d
karafaze <fazli.carrat@gmail.com>
2023-02-24 11:22:37 +0100

    implemented toggle theme button

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ export default function App() {
     const [city, setCity] = useState("");
     const [weatherData, setWeatherData] = useState(null);
     const [error, setError] = useState(null)
+    // const [theme, setTheme] = useState('dark');
+    const [theme, setTheme] = useState('dark');
+
     const getWeatherData = async (e) => {
         e.preventDefault();
-        const response = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather/?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
-        );
-        const data = await response.json();
-        if (data.cod === '404'){
-            setWeatherData(null)
-            setError(data.message)
-            setCity('')
+        const cityName = city.trim()
+        if (cityName === ''){
+            setError('You need to enter a city name before submitting.')
         } else {
-            setWeatherData(data);
-            setCity('')
-            setError(null)
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather/?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+            );
+            const data = await response.json();
+            if (data.cod === '404'){
+                setWeatherData(null)
+                setError('Sorry the city your entered doet not seem to exists.')
+                setCity('')
+            } else {
+                setWeatherData(data);
+                setCity('')
+                setError(null)
+            }
         }
     };
 
@@ -31,26 +39,49 @@ export default function App() {
         return `${clientTime.getHours()}h${clientTime.getMinutes()}`
     };
 
+    const toggleTheme = () => {
+        setTheme(prevTheme => {
+            return prevTheme === 'dark' ? 'warm' : 'dark'
+        })
+    }
+
+    const toggleBtnStyle = {
+        marginRight: theme === 'dark' ? 'auto' : 0,
+        marginLeft: theme === 'warm' ? 'auto' : 0,
+        padding: theme === 'dark' ? '0 .05em 0 0' : '0 0.05em 0 0',
+    }
+
     return (
-        <main className="main--container">
-            <form className="form">
-                <input
-                    placeholder="Type a city..."
-                    id="city"
-                    className={`form--input ${error ? 'form--input__error' : ''}`}
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)}
-                />
-                <button onClick={getWeatherData} className={`form--btn ${error ? 'form--btn__error' : ''}`}>
-                    <i className="ri-search-line"></i>
-                </button>
-            </form>
-            { error ? <p className="error-search">Sorry the city your enter does not seems to exists</p> : null}
+        <main className={`main--container ${theme === 'warm' ? 'main--container__warm': ''}`}>
+            <div className="top-section">
+                <form className="form">
+                    <input
+                        placeholder="Type a city..."
+                        id="city"
+                        className={`form--input ${error ? 'form--input__error' : ''} ${theme === 'warm' ? 'form--input__warm': ''}`}
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+                    <button onClick={getWeatherData} className={`form--btn ${error ? 'form--btn__error' : ''} ${theme === 'warm' ? 'form--btn__warm': ''}`}>
+                        <i className="ri-search-line"></i>
+                    </button>
+                </form>
+                <div 
+                    onClick={toggleTheme}
+                    className={`toggle-theme--wrapper ${theme === 'warm' ? 'toggle-theme--wrapper__warm' : ''}`}
+                >
+                    <div
+                        style={toggleBtnStyle} 
+                        className={`toggle-theme--btn ${theme === 'warm' ? 'toggle-theme--btn__warm' : ''}`}
+                    ></div>
+                </div>
+            </div>
+            { error ? <p className="error-search">{error}</p> : null}
             {
                 weatherData ? (
                     <section className="weather-list">
-                        <article className="weather--single">
-                            <div className="weather--main">
+                        <article className={`weather--single ${theme === 'warm' ? 'weather--single__warm': ''}`}>
+                            <div className={`weather--main ${theme === 'warm' ? 'weather--main__warm': ''}`}>
                                 <p className="weather--main__global">{weatherData.weather[0].main}</p>
                                 <div className="weather--main__degree">
                                     <p>{weatherData.main.temp.toFixed(1)}°C</p>
@@ -61,7 +92,7 @@ export default function App() {
                                 <h2 className="weather--city">{weatherData.name}</h2>
                                 <h3 className="weather--time">Currently : {getTime(weatherData.timezone)}</h3>
                             </div>
-                            <div className="weather--additionnal">
+                            <div className={`weather--additionnal ${theme === 'warm' ? 'weather--additionnal__warm': ''}`}>
                                 <p>Wind : {weatherData.wind.speed}</p>
                                 <p>Clouds : {weatherData.clouds.all}</p>
                                 <p>Humidity : {weatherData.main.humidity}</p>
